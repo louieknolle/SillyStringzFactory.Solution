546fdf21d7fe196009a9694f01df6fead8840fe4
Louis Knolle <knollelw@gmail.com>
2022-08-12 13:47:17 -0400

    complete initial models files

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    public string Brand { get; set; }
+    public string Name { get; set; }
+    public int PurchaseYear { get; set; }
+    public string Description { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get;}
+  }
+}
